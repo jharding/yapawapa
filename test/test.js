eea430e5ff54ad9629a65183c84303316d32d06a
Jake Harding <jharding@twitter.com>
2012-12-23 08:39:47 -0800

    Add some tests.

---
+++ test/test.js
@@ -0,0 +1,93 @@
+ var _ = require('underscore')
+  , assert = require('assert')
+  , sinon = require('sinon')
+  , nextTick = process.nextTick
+  , yapawapa = require('../');
+
+describe('Yapawapa', function() {
+  var mockContext = { _test_: true }
+    , errors = {
+        sync: new Error('sync error')
+      , async: new Error('async error')
+      }
+    , funcs = {
+        fn1: function() {}
+      , fn2: function() {}
+      , sync: function() { return 'sync'; }
+      , async: function(cb) { nextTick(function() { cb(null, 'async'); }) }
+      , syncFail: function() { throw errors.sync; }
+      , asyncFail: function(cb) { nextTick(function() { cb(errors.async); }) }
+      , getContext: function() { return this; }
+      , getArgs: function(arg1, arg2) { return [].slice.call(arguments, 0); }
+      }
+    , Promise = yapawapa(funcs);
+
+  it('should add wrapped methods to Promise prototype', function() {
+    assert.equal(typeof Promise.prototype.fn1, 'function');
+    assert.equal(typeof Promise.prototype.fn2, 'function');
+  });
+
+  it('should return Promise instance when wrapped fn called', function() {
+    assert((new Promise().fn1()) instanceof Promise);
+  });
+
+  it('should call fulfillment handler with correct value', function(done) {
+    var promise = new Promise();
+
+    promise
+    .sync()
+    .then(function(val) { assert.equal(val, 'sync'); })
+    .async()
+    .then(function(val) { assert.equal(val, 'async'); done(); });
+
+    promise.resolve();
+  });
+
+  it('should propogate error', function(done) {
+    var promise = new Promise();
+
+    promise
+    .syncFail()
+    .fn1()
+    .then(function() { assert(false); })
+    .error(function(err) { assert.equal(err.message, errors.sync.message); })
+    .asyncFail()
+    .fn1()
+    .then(function() { assert(false); })
+    .error(function(err) {
+      assert.equal(err.message, errors.async.message);
+      done();
+    })
+
+    promise.resolve();
+  });
+
+  it('should call methods in correct context', function(done) {
+    var promise = new Promise({ context: mockContext });
+
+    promise
+    .getContext()
+    .then(function(context) {
+      assert.strictEqual(context, mockContext);
+      done();
+    });
+
+    promise.resolve();
+  });
+
+  it('should call methods with correct arguments', function(done) {
+    var promise = new Promise()
+      , arg1 = 'i am arg1'
+      , arg2 = 'i am arg2';
+
+    promise
+    .getArgs(arg1, arg2)
+    .then(function(args) {
+      assert.equal(args[0], arg1);
+      assert.equal(args[1], arg2);
+      done();
+    });
+
+    promise.resolve();
+  });
+});
